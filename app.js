e401539afe25ac33172c9b1ecb821fc74c9177a4
andres beyra <andresbeyra@gmail.com>
2021-07-01 19:45:22 -0400

    removed favicon code and wrote process object in lowercase form like i was supposed to

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post("/failure.html", function(req, res){
   res.redirect("/");
 });
 
-app.listen(Process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Port 3000 online");
 });
 
